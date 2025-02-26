1bafead296de311f57d058b5f66d07a8adda24ac
Meryl Ngoc <merylngo@mu-l2.yar.21-school.ru>
2025-02-26 14:10:32 +0300

    0.2 commit

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
-#include <stdio.h>
+#include <ncurses.h>
 
 int main(void) {
-  printf("hello");
+  int x = 20, y = 10;
+  int c = 0;
+
+  initscr();
+  keypad(stdscr, 1);
+  noecho();
+  curs_set(0);
+
+  do {
+    clear();
+
+    switch (c) {
+      case KEY_UP:
+        y--;
+        break;
+      case KEY_DOWN:
+        y++;
+        break;
+      case KEY_LEFT:
+        x--;
+        break;
+      case KEY_RIGHT:
+        x++;
+        break;
+      default:
+        continue;
+    }
+
+    mvaddch(y, x, '@');
+  } while ((c = getch()) && c != 27);  // 27 = ESC
+
+  endwin();
 
   return 0;
 }
