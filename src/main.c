8b166aa65eb9349896fb4bdaa4a22120cf4919b5
Meryl Ngoc <merylngo@mu-l2.yar.21-school.ru>
2025-02-26 19:19:48 +0300

    0.4 commit

--- src/main.c
+++ src/main.c
@@ -2,6 +2,14 @@
 
 #define FIELD_SIZE_Y 20
 #define FIELD_SIZE_X 20
+#define BLOCK_SIZE 4
+
+// typedef struct game_state_t
+// {
+//   int field_matrix[FIELD_SIZE_X][FIELD_SIZE_Y];
+//   int current_block[BLOCK_SIZE][BLOCK_SIZE];
+// } game_state_t;
+
 
 void init_field(char field[FIELD_SIZE_X + 2][FIELD_SIZE_Y + 2]);
 void print_field_with_x_y(char field[FIELD_SIZE_X + 2][FIELD_SIZE_Y + 2], int y,
@@ -21,8 +29,21 @@ void init_field(char field[FIELD_SIZE_X + 2][FIELD_SIZE_Y + 2]) {
   }
 }
 
+
 void print_field_with_x_y(char field[FIELD_SIZE_X + 2][FIELD_SIZE_Y + 2], int y,
                           int x) {
+   for (int i = 0; i <= FIELD_SIZE_X + 1; i++) {
+    for (int j = 0; j <= FIELD_SIZE_Y + 1; j++) {
+      if (i == 0 || i == FIELD_SIZE_X + 1) {
+        field[i][j] = '|';
+      } else if (j == 0 || j == FIELD_SIZE_Y + 1) {
+        field[i][j] = '-';
+      } else {
+        field[i][j] = ' ';
+      }
+    }
+  }
+
   for (int i = 0; i <= FIELD_SIZE_X + 1; i++) {
     for (int j = 0; j <= FIELD_SIZE_Y + 1; j++) {
       mvaddch(y + j, x + i, field[i][j]);
@@ -34,14 +55,19 @@ void init_field(char field[FIELD_SIZE_X + 2][FIELD_SIZE_Y + 2]);
 
 int main(void) {
   int x0 = 10, y0 = 10, x = 15, y = 15;
-  int c = 0;
+  int command = 0;
   // int cols, rows;
 
   initscr();
   keypad(stdscr, 1);
   noecho();
   curs_set(0);
-  // getmaxyx(stdscr, rows, cols);
+  // getmaxyx(stdscr, rows, cols);  
+
+   WINDOW *local_win;
+
+    local_win = newwin(20, 20, 10, 10); // Создание нового окна
+    box(local_win, 0, 0);
 
   char field[FIELD_SIZE_X + 2][FIELD_SIZE_Y + 2];
 
@@ -53,25 +79,25 @@ int main(void) {
 
     print_field_with_x_y(field, y0, x0);
 
-    if (c == KEY_UP && y > y0 + 1) {
-      y--;
-    }
+    mvwprintw(local_win, 1, 1, "HELLO ");
+    //wrefresh(local_win);
 
-    if (c == KEY_DOWN && y < y0 + FIELD_SIZE_Y) {
+    if (command == KEY_DOWN && y < y0 + FIELD_SIZE_Y) {
       y++;
     }
 
-    if (c == KEY_LEFT && x > x0 + 1) {
+    if (command == KEY_LEFT && x > x0 + 1) {
       x--;
     }
 
-    if (c == KEY_RIGHT && x < x0 + FIELD_SIZE_X) {
+    if (command == KEY_RIGHT && x < x0 + FIELD_SIZE_X) {
       x++;
     }
 
     mvaddch(y, x, '@');
-  } while ((c = getch()) && c != 27);  // 27 = ESC
+  } while ((command = getch()) && command != 27);  // 27 = ESC
 
+  delwin(local_win);
   endwin();
 
   return 0;
